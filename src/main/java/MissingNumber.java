4f7247c31dc5bf198272db610cef1bf0b710c540
Salaia <puma.hope@yandex.ru>
2024-09-24 15:11:59 +0300

    add: new approaches, tests

--- src/main/java/MissingNumber.java
+++ src/main/java/MissingNumber.java
@@ -23,6 +23,37 @@ public class MissingNumber {
     }
 
     /*
+    Or we can use a frequency array.
+    Again, we can do so in case sequence is 0,1,2,3...N with missing member
+     */
+    public int findMissingNumberViaFrequencyArray(int[] nums) {
+        int[] frequency = new int[nums.length + 1];
+        for (int i = 0; i < nums.length; i++) {
+            frequency[nums[i]]++;
+        }
+        for (int i = 0; i < frequency.length; i++) {
+            if (frequency[i] == 0) return i;
+        }
+        return -1;
+    }
+
+    /*
+    Another approach is to sum up indexes
+     */
+    public int findMissingNumberViaIndexSum(int[] nums) {
+        int IndexSum = 0;
+        int valuesSum = 0;
+        for (int i = 0; i <= nums.length; i++) {
+            IndexSum += i;
+        }
+        for (int i = 0; i < nums.length; i++) {
+            valuesSum += nums[i];
+        }
+        return IndexSum - valuesSum;
+    }
+
+
+    /*
     Example 2 gives us [7, 9,10, 11, 12] sequence, that doesn't start with 0.
     Again, this can be solved via natural sum. Just add the natural sum of ghost prev-numbers
      */
@@ -32,14 +63,15 @@ public class MissingNumber {
 
         for (int num : numbers) {
             actualSum += num;
-            if(num < min) {
+            // can be replaced with    min = Math.min(min,num);
+            if (num < min) {
                 min = num;
             }
         }
 
         //add natural sum for non-existing previous part of array
         int prevNumber = min - 1;
-        actualSum += prevNumber*(prevNumber+1)/2;
+        actualSum += prevNumber * (prevNumber + 1) / 2;
 
         int alteredLength = numbers.length + min;
         int naturalSum = alteredLength * (alteredLength + 1) / 2;
@@ -52,16 +84,31 @@ public class MissingNumber {
     "Function finds a first missing number occurrence in the sequence"
     Wow, so there might be several missing numbers,
     and we need to find the first occurrence?..
-    Well then, it's definitely work for sorting!
+    Unfortunately, it's impossible as the incoming sequence isn't sorted.
+    Yet we can sort it and find the minimum (or the maximum, if you like) missing number.
      */
-    public int findFirstMissingNumber(int[] numbers) {
+    public int findMinimumMissingNumberAmongUnique(int[] numbers) {
         Arrays.sort(numbers); // or we can sort manually
         for (int i = 1; i < numbers.length; i++) {
-            if (numbers[i] != numbers[i-1]+1) {
-                return numbers[i]-1;
+            if (numbers[i] != numbers[i - 1] + 1) {
+                return numbers[i] - 1;
             }
         }
         return -1; // this is not meant to happen if there is at least one missing number, throw respective exception in prod
     }
 
+    /*
+        Did I say it was the last challenge?
+        Nope, we're not told we have a sequence of unique numbers.
+        So, what if they're not unique?
+     */
+    public int findMinimumMissingNumberAmongNonUnique(int[] numbers) {
+        Arrays.sort(numbers);
+        for (int i = 1; i < numbers.length; i++) {
+            if (numbers[i] != numbers[i - 1] && numbers[i - 1] + 1 != numbers[i]) {
+                return numbers[i - 1] + 1;
+            }
+        }
+        return -1; // this is not meant to happen if there is at least one missing number, throw respective exception in prod
+    }
 }
--- src/test/java/MissingNumberTest.java
+++ src/test/java/MissingNumberTest.java
@@ -1,6 +1,7 @@
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 public class MissingNumberTest {
 
@@ -8,15 +9,31 @@ public class MissingNumberTest {
 
     @Test
     public void findMissingNumberViaNaturalSum() {
-        int[] input = {5,0,1,3,2};
+        int[] input = {5, 0, 1, 3, 2};
         int expected = 4;
         int output = mn.findMissingNumberNaturalZero(input);
         assertEquals(expected, output);
     }
 
     @Test
+    public void findMissingNumberViaFrequencyArray() {
+        int[] input = {5, 0, 1, 3, 2};
+        int expected = 4;
+        int output = mn.findMissingNumberViaFrequencyArray(input);
+        assertEquals(expected, output);
+    }
+
+    @Test
+    public void findMissingNumberViaIndexSum() {
+        int[] input = {5, 0, 1, 3, 2};
+        int expected = 4;
+        int output = mn.findMissingNumberViaIndexSum(input);
+        assertEquals(expected, output);
+    }
+
+    @Test
     public void failFindMissingNumberViaNaturalSum() {
-        int[] input = {7, 9,10, 11, 12};
+        int[] input = {7, 9, 10, 11, 12};
         int expected = 8;
         int output = mn.findMissingNumberNaturalZero(input);
         assertNotEquals(expected, output);
@@ -24,7 +41,7 @@ public class MissingNumberTest {
 
     @Test
     public void findMissingNumberInRandomSequence() {
-        int[] input = {7, 9,10, 11, 12};
+        int[] input = {7, 9, 10, 11, 12};
         int expected = 8;
         int output = mn.findMissingNumberRandomSequenceViaNaturalSum(input);
         assertEquals(expected, output);
@@ -32,9 +49,17 @@ public class MissingNumberTest {
 
     @Test
     public void findMissingNumberInRandomSequenceBySort() {
-        int[] input = {997,995,994,992,991};
+        int[] input = {997, 995, 994, 992, 991};
         int expected = 993;
-        int output = mn.findFirstMissingNumber(input);
+        int output = mn.findMinimumMissingNumberAmongUnique(input);
+        assertEquals(expected, output);
+    }
+
+    @Test
+    public void findMissingNumberInNonUniqueRandomSequenceBySort() {
+        int[] input = {997, 995, 994, 992, 991, 995, 992};
+        int expected = 993;
+        int output = mn.findMinimumMissingNumberAmongNonUnique(input);
         assertEquals(expected, output);
     }
 }
